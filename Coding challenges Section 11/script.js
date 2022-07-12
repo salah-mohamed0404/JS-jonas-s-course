26d0669aafdeaa54dc8a5e347194e716b8123151
salahcst344 <salahcst344@gmail.com>
2022-07-12 16:56:23 +0200

    solve coding challenge 1

--- Coding challenges Section 11/script.js
+++ Coding challenges Section 11/script.js
@@ -1,15 +1,48 @@
 "use strict";
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
+// Coding Challenge #1
 
-const currencies = new Map([
-  ["USD", "United States dollar"],
-  ["EUR", "Euro"],
-  ["GBP", "Pound sterling"],
-]);
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
 
-/////////////////////////////////////////////////
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+// My Solution
+
+/*
+const checkDogs = function (jDogs, kDogs) {
+  const jDogsEdit = jDogs.slice(1, -2);
+  // console.log(jDogsEdit);
+  const dogs = [...jDogsEdit, ...kDogs];
+
+  dogs.forEach(function (age, i) {
+    console.log(
+      `Dog number ${i + 1} is ${
+        age >= 3 ? "an adult" : "a puppy 🐶"
+      }, and is ${age} years old`
+    );
+  });
+};
+
+let dogsJulia = [3, 5, 2, 12, 7];
+let dogsKate = [4, 1, 15, 8, 3];
+checkDogs(dogsJulia, dogsKate);
+
+console.log("-------TEST 2--------");
+
+dogsJulia = [9, 16, 6, 8, 3];
+dogsKate = [10, 5, 6, 1, 4];
+checkDogs(dogsJulia, dogsKate);
+*/
