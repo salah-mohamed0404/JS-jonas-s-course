e43d62066a803a5d8d1fc51f5d5d0254ddb6227b
salahcst344 <salahcst344@gmail.com>
2022-07-14 19:58:08 +0200

    implement closing account

--- Bank list/script.js
+++ Bank list/script.js
@@ -172,3 +172,21 @@ btnTransfer.addEventListener("click", function (e) {
     updateUI(currentAccount);
   }
 });
+
+btnClose.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+
+    accounts.splice(index, 1);
+
+    containerApp.style.opacity = 0;
+  }
+  inputClosePin.value = inputCloseUsername.value = "";
+});
