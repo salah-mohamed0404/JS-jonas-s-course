42011b4f36188f914d66c8ea1c44267c2c81026f
salahcst344 <salahcst344@gmail.com>
2022-07-15 13:19:47 +0200

    loan functionalty

--- Bank list/script.js
+++ Bank list/script.js
@@ -173,6 +173,17 @@ btnTransfer.addEventListener("click", function (e) {
   }
 });
 
+btnLoan.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const amount = inputLoanAmount.value;
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    currentAccount.movements.push(amount);
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = "";
+});
+
 btnClose.addEventListener("click", function (e) {
   e.preventDefault();
 
