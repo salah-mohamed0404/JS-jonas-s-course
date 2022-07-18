fd2f907b68d12dc0c0f7f32f52f2f2b44e230496
salahcst344 <salahcst344@gmail.com>
2022-07-18 15:11:40 +0200

    Implementing tabbed component

--- Bankist Website/script.js
+++ Bankist Website/script.js
@@ -6,6 +6,9 @@ const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
 
 ///////////////////////////////////////
 // Modal window
@@ -78,4 +81,26 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
     // console.log(id);
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
+});
+
+///////////////////////////////////////
+//  Tabbed component
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+  // console.log(clicked);
+
+  // Guard clause
+  if (!clicked) return;
+
+  // remove active classes
+  tabs.forEach(t => t.classList.remove("operations__tab--active"));
+  tabsContent.forEach(c => c.classList.remove("operations__content--active"));
+
+  // activate tab
+  clicked.classList.add("operations__tab--active");
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
 });
