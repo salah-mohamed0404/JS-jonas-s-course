10bc2d4b35db864064445acca6e29a1150c61a1a
salahcst344 <salahcst344@gmail.com>
2022-07-18 15:31:49 +0200

    Implementing menu fade animation

--- Bankist Website/script.js
+++ Bankist Website/script.js
@@ -4,11 +4,15 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
+
 const tabs = document.querySelectorAll(".operations__tab");
 const tabsContainer = document.querySelector(".operations__tab-container");
 const tabsContent = document.querySelectorAll(".operations__content");
+
+const nav = document.querySelector(".nav");
 
 ///////////////////////////////////////
 // Modal window
@@ -104,3 +108,23 @@ tabsContainer.addEventListener("click", function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add("operations__content--active");
 });
+
+// menu fade animation
+// mouseenter not bubble so mouseover is better here
+
+const handleHove = function (e) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener("mouseover", handleHove.bind(0.5));
+
+nav.addEventListener("mouseout", handleHove.bind(1));
