4bff0fe20b42ca4c45342ce662f99713f0537bc6
salahcst344 <salahcst344@gmail.com>
2022-07-21 21:51:08 +0200

    Refactoring

--- Coding challenges Sec 16/script.js
+++ Coding challenges Sec 16/script.js
@@ -7,17 +7,9 @@ const countriesContainer = document.querySelector('.countries');
 // https://restcountries.com/v2/
 ///////////////////////////////////////
 
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-
-  request.open('GET', `https://restcountries.com/v2/name/${country}`);
-  request.send();
-
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-
-    const html = `
-  <article class="country">
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
     <img class="country__img" src="${data.flag}" />
     <div class="country__data">
       <h3 class="country__name">${data.name}</h3>
@@ -30,11 +22,40 @@ const getCountryData = function (country) {
     </div>
 </article>
   `;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+
+    renderCountry(data);
+
+    // Get neighbour country
+    const [neighbour] = data?.borders;
+
+    // ajax call
+    const request2 = new XMLHttpRequest();
+
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
+
 getCountryData('portugal');
+
 search.addEventListener('click', function (e) {
   e.preventDefault();
   getCountryData(countryInput.value);
