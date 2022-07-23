022015ade46fc45d6e3443f7176b37e77f8082bf
salahcst344 <salahcst344@gmail.com>
2022-07-23 20:15:44 +0200

    coding challenge 3

--- Coding challenges Sec 16/script.js
+++ Coding challenges Sec 16/script.js
@@ -177,7 +177,7 @@ PART 2
 TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
 
 GOOD LUCK 😀
-*/
+/*
 const wait = function (seconds) {
   return new Promise(function (resolve) {
     setTimeout(resolve, seconds * 1000);
@@ -209,3 +209,65 @@ createImage("img/img-1.jpg")
   .then(() => wait(2))
   .then(() => (img.style.display = "none"))
   .catch(err => console.error(err));
+*/
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+PART 1
+Write an async function 'loadNPause' that recreates Coding Challenge #2, this time using async/await (only the part where the promise is consumed). Compare the two versions, think about the big differences, and see which one you like more.
+Don't forget to test the error handler, and to set the network speed to 'Fast 3G' in the dev tools Network tab.
+
+PART 2
+1. Create an async function 'loadAll' that receives an array of image paths 'imgArr';
+2. Use .map to loop over the array, to load all the images with the 'createImage' function (call the resulting array 'imgs')
+3. Check out the 'imgs' array in the console! Is it like you expected?
+4. Use a promise combinator function to actually get the images from the array 😉
+5. Add the 'paralell' class to all the images (it has some CSS styles).
+
+TEST DATA: ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']. To test, turn off the 'loadNPause' function.
+
+GOOD LUCK 😀
+*/
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement("img");
+    img.src = imgPath;
+
+    img.addEventListener("load", function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener("error", function () {
+      reject(new Error("Image not found"));
+    });
+  });
+};
+
+const imgContainer = document.querySelector(".images");
+
+const loadNPause = async function () {
+  try {
+    // load first img
+    let img = await createImage("img/img-1.jpg");
+    console.log("image 1 has loaded");
+    await wait(2);
+    img.style.display = "none";
+
+    // load second
+    img = await createImage("img/img-2.jpg");
+    await wait(2);
+    img.style.display = "none";
+  } catch {
+    console.error(err);
+  }
+};
+loadNPause();
