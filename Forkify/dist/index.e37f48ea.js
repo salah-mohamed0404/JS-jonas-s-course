a04babddb336be68d8d46dd494d8cf23788e526f
salahcst344 <salahcst344@gmail.com>
2022-07-28 02:12:27 +0200

    Update results view to mark selected search result

--- Forkify/dist/index.e37f48ea.js
+++ Forkify/dist/index.e37f48ea.js
@@ -553,6 +553,8 @@ const controlRecipes = async function() {
         const id = window.location.hash.slice(1);
         if (!id) return;
         (0, _recipeViewJsDefault.default).renderSpinner();
+        // 0) Update results view to mark selected search result
+        (0, _resultsViewJsDefault.default).update(_modelJs.getSearchResultsPage());
         // 1) loading recipe
         await _modelJs.loadRecipe(id);
         // 2) Rendering recipe
@@ -2758,9 +2760,10 @@ class ResaultView extends (0, _viewJsDefault.default) {
         return this._data.map(this._generateMarkupPreview).join("");
     }
     _generateMarkupPreview(result) {
+        const id = window.location.hash.slice(1);
         return `
       <li class="preview">
-        <a class="preview__link" href="#${result.id}">
+        <a class="preview__link ${result.id === id ? "preview__link--active" : ""}" href="#${result.id}">
           <figure class="preview__fig">
             <img src="${result.image}" alt="Test" />
           </figure>
--- Forkify/src/js/controller.js
+++ Forkify/src/js/controller.js
@@ -18,6 +18,9 @@ const controlRecipes = async function () {
 
     if (!id) return;
     recipeView.renderSpinner();
+
+    // 0) Update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
     // 1) loading recipe
     await model.loadRecipe(id);
     // 2) Rendering recipe
